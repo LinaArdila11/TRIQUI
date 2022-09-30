322f6240825a16d611823490d383654d2034ca35
Lina Ardila <lmardilag@correo.udistrital.edu.co>
2022-09-30 17:48:18 -0500

    V02

--- Tablero.py
+++ Tablero.py
@@ -13,21 +13,21 @@ class Tablero:
 
         if x==-1:
             return False
-            
+
         if self.matriz[x][y]==" ":
             return True
         else:
             return False
     def verificar_triqui(self):
         for fila in range(3):
-            if self.matriz[fila][0]==self.matriz [fila][1] and self.matriz [fila][0]==self.matriz[fila][2]:
+            if self.matriz[fila][0]==self.matriz [fila][1] and self.matriz [fila][0]==self.matriz[fila][2] and not self.matriz[fila][2]:
                 return True
         for columna in range(3):
-            if self.matriz [0][columna]==self.matriz[1][columna] and self.matriz[0][columna]== self.matriz[2][columna]:
+            if self.matriz [0][columna]==self.matriz[1][columna] and self.matriz[0][columna]== self.matriz[2][columna] and not self.matriz[2][columna]:
                 return True
-        if self.matriz[0][0]==self.matriz[1][1] and self.matriz[0][0]==self.matriz[2][2]:
+        if self.matriz[0][0]==self.matriz[1][1] and self.matriz[0][0]==self.matriz[2][2] and not self.matriz[2][2]:
             return True 
-        if self.matriz [0][2]==self.matriz[1][1] and self.matriz [0][2]==self.matriz[2][0]:
+        if self.matriz [0][2]==self.matriz[1][1] and self.matriz [0][2]==self.matriz[2][0] and not self.matriz[2][0]:
             return True
         return False
                      
